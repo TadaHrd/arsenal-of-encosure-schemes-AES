6cb5f758d8564a4c3a56a5be2fbc2ebaa971a43c
tadaHrd <hrudka.tadeas@gmail.com>
2024-04-17 15:25:24 +0200

    Added Ook Encosure Scheme

---
+++ ook encosure scheme/index.js
@@ -0,0 +1,93 @@
+// Written by TadaHrd.
+// If this doesn't work blame him.
+
+let ookTable = {
+    0b0000: "OOK",
+    0b0001: "OOk",
+    0b0010: "OoK",
+    0b0011: "Ook",
+    0b0100: "OOK.",
+    0b0101: "OOk.",
+    0b0110: "OoK.",
+    0b0111: "Ook.",
+    0b1000: "OOK?",
+    0b1001: "OOk?",
+    0b1010: "OoK?",
+    0b1011: "Ook?",
+    0b1100: "OOK!",
+    0b1101: "OOk!",
+    0b1110: "OoK!",
+    0b1111: "Ook!",
+
+    "OOK":  0b0000,
+    "OOk":  0b0001,
+    "OoK":  0b0010,
+    "Ook":  0b0011,
+    "OOK.": 0b0100,
+    "OOk.": 0b0101,
+    "OoK.": 0b0110,
+    "Ook.": 0b0111,
+    "OOK?": 0b1000,
+    "OOk?": 0b1001,
+    "OoK?": 0b1010,
+    "Ook?": 0b1011,
+    "OOK!": 0b1100,
+    "OOk!": 0b1101,
+    "OoK!": 0b1110,
+    "Ook!": 0b1111,
+};
+
+let ook_sep_check_regex = /^[^oOkK?!.]+$/;
+let ook_sep_regex = /[^oOkK?!.]+/;
+
+function ook_encode(input, sep) {
+    let data;
+    if (typeof input == "string")
+        data = textEncoder.encode(input);
+    else
+        data = input;
+
+    if (!ook_sep_check_regex.test(sep) || sep == undefined)
+        sep = "\n";
+
+    let ret = "";
+    for (let val of data) {
+        let nibble0 = (val >> 0) & 0b1111;
+        let nibble1 = (val >> 4) & 0b1111;
+
+        ret += ookTable[nibble1] + sep;
+        ret += ookTable[nibble0] + sep;
+    }
+
+    ret = ret.substring(0, ret.length - sep.length);
+
+    return ret;
+}
+
+function ook_decode(text, return_string) {
+    let ret = [];
+
+    let text_split = text.split(ook_sep_regex);
+    console.log(text_split)
+
+    for (let i = 0; i < text_split.length; i++) {
+        let v0 = text_split[i];
+        let v1 = text_split[i + 1] || ookTable[0];
+
+        console.log(v0, v1)
+
+        i++;
+
+        let nibble0 = ookTable[v0];
+        let nibble1 = ookTable[v1];
+
+        let num = nibble1 + (nibble0 << 4);
+
+        ret.push(num);
+    }
+
+    if (return_string)
+        return textDecoder.decode(new Uint8Array(ret));
+
+    return ret;
+}
---
+++ ook encosure scheme/readme.md
@@ -0,0 +1,67 @@
+# Ook Encosure Scheme (OES)
+
+This encosure scheme uses the word "Ook" with punctuation and capitalization to store data.
+
+Bits in bytes are talked about as LSb to MSb.
+This means that the 1's place bit is bit 0 and the 128's place bit is bit 7.
+Byte sequences start with the MSb first.
+
+Each byte has 2 parts:
+- 1st Nibble (0-3)
+- 2nd Nibble (4-7)
+
+Every nibble has 2 parts:
+- A tail (bits 0-1)
+- A body (bits 2-3)
+
+The tail dictates the punctian after Ook.
+
+| bit 0 | bit 1 | punctuation |
+| ----- | ----- | ----------- |
+| 0     | 0     |             |
+| 1     | 0     | .           |
+| 0     | 1     | ?           |
+| 1     | 1     | !           |
+
+The body dictates the case of the letters in the the `ok` of `Ook`.
+
+| bit | case      |
+| --- | --------- |
+| 0   | uppercase |
+| 1   | lowercase |
+
+The bits control the letters like this:
+
+| letter | bit              |
+| ------ | ---------------- |
+| O      | not controlled   |
+| o      | 0                |
+| k      | 1                |
+
+For example, "A" would be:
+- 65 in ASCII
+- `01000001` in binary
+- Split it into nibbles: `0100 0001`
+- Encoded as: `OOK. OOk`
+
+## Examples
+
+### Hello, world!
+
+The string "Hello, world!" would look like:
+```
+H: OOK. OOK?
+e: OoK. OOk.
+l: OoK. OOK!
+l: OoK. OOK!
+o: OoK. Ook!
+,: OoK OOK!
+ : OoK OOK
+w: Ook. Ook.
+o: OoK. Ook!
+r: Ook. OoK
+l: OoK. OOK!
+d: OoK. OOK.
+```
+
+The characters at the start of every row aren't present in actual encoded data.
